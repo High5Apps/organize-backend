3969ee90a7d37c2695f33f0a25433145898f89b5
Julian Tigler <High5Apps@gmail.com>
2022-09-05 00:04:04 -0700

    Create Pseudonym PORO
    - Allows for a max of 10,000 different pseudonyms

---
+++ app/models/users/pseudonym.rb
@@ -0,0 +1,34 @@
+class Users::Pseudonym
+  attr_reader :adjectives, :animals
+
+  def initialize(seed, adjectives: nil, animals: nil)
+    @seed = seed
+    @adjectives = adjectives || Users::Pseudonyms::Adjectives::LIST_OF_100
+    @animals = animals || Users::Pseudonyms::Animals::LIST_OF_100
+  end
+
+  def at(index)
+    i = shuffled_indices[index]
+    adjective_i = i / @adjectives.length
+    animal_i = i % @animals.length
+    "#{@adjectives[adjective_i]} #{@animals[animal_i]}"
+  end
+
+  def to_a
+    shuffled_indices.map { |i| at(i) }
+  end
+
+  def inspect
+    "<#{self.class} @seed=#{@seed}>"
+  end
+
+  private
+
+  def shuffled_indices
+    return @shuffled_indices if @shuffled_indices
+
+    size = @adjectives.length * @animals.length
+    @shuffled_indices = (0...size).to_a
+    @shuffled_indices.shuffle! random: Random.new(@seed)
+  end
+end
---
+++ test/models/user_test/pseudonym_test.rb
@@ -0,0 +1,62 @@
+require "test_helper"
+
+class PseudonymTest < ActiveSupport::TestCase
+  SEED = 12345
+  TEST_ADJECTIVES = [
+    'Adorable',
+    'Adventurous',
+    'Agreeable',
+  ]
+  TEST_ANIMALS = [
+    'Alpaca',
+    'Ant',
+    'Antelope',
+  ]
+  TEST_COMBINATION_COUNT = TEST_ADJECTIVES.length * TEST_ANIMALS.length
+
+  setup do
+    @real_pseudonym = Users::Pseudonym.new(SEED)
+    @create_test_pseudonym = ->(seed) do
+      Users::Pseudonym.new(
+        seed, 
+        adjectives: TEST_ADJECTIVES,
+        animals: TEST_ANIMALS)
+    end
+    @test_pseudonym = @create_test_pseudonym.call(SEED)
+  end
+
+  test 'default animals list should contain 100 unique entries' do
+    assert_equal 100, @real_pseudonym.animals.length
+  end
+
+  test 'default adjectives list should contain 100 unique entries' do
+    assert_equal 100, @real_pseudonym.adjectives.length
+  end
+
+  test 'should contain all combinations' do
+    assert_equal TEST_COMBINATION_COUNT, @test_pseudonym.to_a.length
+  end
+
+  test 'should contain no duplicates' do
+    assert_equal TEST_COMBINATION_COUNT, @test_pseudonym.to_a.uniq.length
+  end
+
+  test 'should be stable when given the same seed' do
+    pseudonym_1 = @create_test_pseudonym.call(SEED)
+    pseudonym_2 = @create_test_pseudonym.call(SEED)
+    assert_equal pseudonym_1.to_a, pseudonym_2.to_a
+  end
+
+  test 'should not return the same list for different seeds' do
+    pseudonym_1 = @create_test_pseudonym.call(1)
+    pseudonym_2 = @create_test_pseudonym.call(2)
+    assert_not_equal pseudonym_1.to_a, pseudonym_2.to_a
+  end
+
+  test 'should format as "Adjective Animal"' do
+    pseudonym = @test_pseudonym.at(0)
+    adjective, animal = pseudonym.split ' '
+    assert TEST_ADJECTIVES.include?(adjective)
+    assert TEST_ANIMALS.include?(animal)
+  end
+end
