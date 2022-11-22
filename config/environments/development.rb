37b460c862ca0e48578882bb7dded944373124ee
Julian Tigler <High5Apps@gmail.com>
2022-11-22 01:29:19 -0800

    Allow connections from http://getorganize.app
        - Find home IP address at 10.0.0.1 > Connection > Status
        - Add A record on NameCheap with host @, Value of IP address
        - Use Xfinity app to set up a port forward to org on 80
        - Run `curl http://getorganize.app`

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Allow requests from domain 'raspberrypi.local'
-  config.hosts << 'raspberrypi.local'
+  # Allow requests from domain 'getorganize.app'
+  config.hosts << 'getorganize.app'
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
