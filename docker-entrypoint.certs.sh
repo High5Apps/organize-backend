a88860621ebf144e523e1ad110aae89dd9fed836
Julian Tigler <High5Apps@gmail.com>
2022-11-28 23:40:06 -0800

    Add certs container that makes and shares certs with web
    - Certbot container volume sharing setup
      - https://mindsers.blog/post/https-using-nginx-certbot-docker/
    - Certbot auto-renewal
      - https://pentacent.medium.com/nginx-and-lets-encrypt-with-docker-in-less-than-5-minutes-b4b8a60d3a71
    - Create temporary self-signed certs to overcome "chicken and egg" problem
      - https://github.com/vdhpieter/docker-letsencrypt-webroot/blob/master/start.sh#L112
    - Background sleep then wait explanation
      - https://stackoverflow.com/questions/56981892/why-do-sleep-wait-in-bash

---
+++ docker-entrypoint.certs.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -e
+
+DOMAIN="getorganize.app"
+EMAIL_ADDRESS="high5apps@gmail.com"
+WEBROOT_PATH="/var/www/certbot/"
+cert_dir="/etc/letsencrypt/live/$DOMAIN"
+cert_file="$cert_dir/fullchain.pem"
+key_file="$cert_dir/privkey.pem"
+
+if ! [[ -e $cert_file ]]; then
+  echo "Certificate file not found for domain $DOMAIN"
+
+  echo "Making a temporary self-signed certificate because nginx expects a cert"
+  mkdir -p $cert_dir
+  openssl req \
+    -x509 \
+    -newkey rsa:4096 \
+    -sha256 \
+    -nodes \
+    -keyout $key_file \
+    -out $cert_file \
+    -subj "/CN=$DOMAIN" \
+    -days 1
+
+  echo "Waiting for web server to restart with self-signed certs..."
+  sleep 10 # This seems to be enough but may need to be raised in the future
+
+  echo "Removing self-signed certs..."
+  rm -rf $cert_dir
+
+  echo "Running certbot to get real certs..."
+  certbot certonly \
+    --agree-tos \
+    --domain $DOMAIN \
+    --email ${EMAIL_ADDRESS} \
+    --non-interactive \
+    --renew-by-default \
+    --webroot \
+    --webroot-path ${WEBROOT_PATH}
+fi
+
+trap exit TERM
+while true; do
+  echo "Waiting 24h until next renewal attempt..."
+  sleep 24h &
+  wait ${!}
+  echo "Running certbot renew..."
+  certbot renew
+done
