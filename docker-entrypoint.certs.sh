cc646a34f305c73616799b1d50746328c700498c
Julian Tigler <High5Apps@gmail.com>
2022-12-15 00:55:31 -0800

    Fix initial server bootstrap failed to validate domain
    - The router port forwards usually need to be reset when bootstrapping a new server
    - Previously, the certs service didn't:
      - restart if it failed
      - re-request a real cert if a self-signed cert already existed
        - This could happen if requesting the real cert previously failed, like if port-forwards were set up and domain validation failed
    - Now the certs service can restart and re-request real certs indefinitely, without hitting the Let's Encrypt rate limits

--- docker-entrypoint.certs.sh
+++ docker-entrypoint.certs.sh
@@ -8,7 +8,17 @@ cert_dir="/etc/letsencrypt/live/$DOMAIN"
 cert_file="$cert_dir/fullchain.pem"
 key_file="$cert_dir/privkey.pem"
 
-if ! [ -e $cert_file ] || [ "$SELF_SIGN" = true ]; then
+set +e
+2>/dev/null openssl x509 -in $cert_file -text -noout \
+  | grep -q "Issuer: CN = $DOMAIN"
+if [ $? -eq 0 ]; then
+  echo "WARNING: Self-signed cert found at $cert_file"
+  echo "Deleting the self-signed cert..."
+  rm -f $cert_file
+fi
+set -e
+
+if ! [ -e $cert_file ]; then
   echo "Making a temporary self-signed certificate because nginx expects a cert"
   mkdir -p $cert_dir
   openssl req \
@@ -25,8 +35,11 @@ if ! [ -e $cert_file ] || [ "$SELF_SIGN" = true ]; then
     exit 0
   fi
 
+  # Waiting 40 seconds because Let's Encrypt has a rate limit of 300 certificate
+  # requests every 3 hours or 1 certificate request every 36 seconds
+  # For more info, see "New Orders" in https://letsencrypt.org/docs/rate-limits/
   echo "Waiting for web server to restart with self-signed certs..."
-  sleep 10 # This seems to be enough but may need to be raised in the future
+  sleep 40 # This seems to be enough but may need to be raised in the future
 
   echo "Removing self-signed certs..."
   rm -rf $cert_dir
