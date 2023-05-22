0e0e445863457f65adf77ce8c86973b2df01fc2b
Julian Tigler <High5Apps@gmail.com>
2023-05-22 21:56:44 +0300

    Add random name and member definition to Org seeds
    - Use top 100 retailers of 2022

--- app/db/seeds/020_create_org.rb
+++ app/db/seeds/020_create_org.rb
@@ -3,8 +3,19 @@ start_time = Time.now
 
 creator = User.first
 
-org = creator.create_org! name: 'Org',
-  potential_member_definition: 'Definition',
+random_local_number = rand 1000..9999
+random_store_number = rand 100..999
+
+def random_company_name
+  companies_string = "Walmart,Amazon.com,Costco Wholesale,The Home Depot,The Kroger Co.,Walgreens Boots Alliance,Target,CVS Health Corporation,Lowe's Companies,Albertsons Companies,Apple Stores / iTunes,Royal Ahold Delhaize USA,Publix Super Markets,Best Buy,TJX Companies,Aldi,Dollar General,H.E. Butt Grocery,Dollar Tree,Ace Hardware,Macy's,7-Eleven,AT&T Wireless,Meijer,Verizon Wireless,Ross Stores,Kohl's,Wakefern / ShopRite,Rite Aid,BJ's Wholesale Club,Dell Technologies,Gap,Nordstrom,Menards,O’Reilly Auto Parts,Tractor Supply Co.,AutoZone,Dick's Sporting Goods,Hy Vee,Wayfair,Health Mart Systems,Wegmans Food Market,Qurate Retail,Giant Eagle,Alimentation Couche-Tard,Sherwin-Williams,Burlington,J.C. Penney Company,WinCo Foods,Chewy.com,Good Neighbor Pharmacy,Ulta Beauty,Williams-Sonoma,Army and Air Force Exchange Service,PetSmart,Bass Pro,Bath & Body Works,Southeastern Grocers,AVB Brandsource,Academy Sports,Staples,Dillard's,Hobby Lobby Stores,Bed Bath & Beyond,Big Lots,Signet Jewelers,Foot Locker,Sprouts Farmers Market,Sephora (LVMH),Ikea North America Services,Discount Tire,Camping World,Petco,True Value Co.,Office Depot,Victoria's Secret,Michaels Stores,Piggly Wiggly,Stater Bros Holdings,My Demoulas,Advance Auto,Harbor Freight Tools,Exxon Mobil Corporation,Hudson's Bay,Save-A-Lot,American Eagle Outfitters,Total Wine & More,Defense Commissary Agency,Ingles,Weis Markets,Casey's General Store,Tapestry,Smart & Final,Lululemon,Shell Oil Company,Golub,Save Mart,RH,Urban Outfitters,Barnes & Noble"
+  companies_string.split(',').sample
+end
+
+potential_member_definition =
+  "An employee of #{random_company_name} at store ##{random_store_number}"
+
+org = creator.create_org! name: "Local #{random_local_number}",
+  potential_member_definition: potential_member_definition,
   potential_member_estimate: $simulation.to_seed_data[:size]
 
 creator.save!
