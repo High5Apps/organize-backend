9e8a635b7c6a49d9f057a307a52a0d07b1aa5c7d
Julian Tigler <High5Apps@gmail.com>
2023-10-22 14:39:39 +0300

    Move seed files into rake tasks subfolder
    - Move app/db/seeds/* to app/lib/tasks/org/simulation/seeds/*

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -5,6 +5,8 @@ $simulation = Simulation.new
 $simulation.run
 $user_id_map = {}
 
-Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each { |seed| load seed }
+Dir[
+  File.join(Rails.root, 'lib', 'tasks', 'org', 'simulation', 'seeds', '*.rb')
+].sort.each { |seed| load seed }
 
 puts "Created seeds. Completed in #{(Time.now - start_time).round 3} s"
