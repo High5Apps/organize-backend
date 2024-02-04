d343c0bf01992140323caf03c7f968284659f46c
Julian Tigler <High5Apps@gmail.com>
2024-02-04 18:00:50 +0200

    Simulate multiple choice ballots
    - Simulate ballots and candidates
    - Votes was already working

--- app/lib/tasks/org/simulation/seeds/090_create_ballots.rb
+++ app/lib/tasks/org/simulation/seeds/090_create_ballots.rb
@@ -4,20 +4,22 @@ BALLOT_DISTRIBUTION_MAX = 30
 
 BALLOTS_DISTRIBUTION = -> { [(1/rand).round, BALLOT_DISTRIBUTION_MAX].min }
 
-QUESTION_PREFIX = 'Should we '
+QUESTION_PREFIXES = {
+  multiple_choice: 'Which of these should we ',
+  yes_no: 'Should we ',
+}
 QUESTION_SUFFIX = '?'
-question_range_max = \
-  Ballot::MAX_QUESTION_LENGTH - QUESTION_PREFIX.length - QUESTION_SUFFIX.length
-QUESTION_CHARACTER_RANGE = 20..question_range_max
 
-def hipster_ipsum_ballot_question
-  question_length = rand QUESTION_CHARACTER_RANGE
+def hipster_ipsum_ballot_question(prefix, suffix)
+  question_range_max = \
+    Ballot::MAX_QUESTION_LENGTH - prefix.length - suffix.length
+  question_length = rand 20..question_range_max
   question = Faker::Hipster.paragraph_by_chars(characters: question_length)
     .delete('.') # Remove all periods
     .downcase
     .split[...-1] # Remove last word to ensure no partial words
     .join ' '
-  "#{QUESTION_PREFIX}#{question}#{QUESTION_SUFFIX}"
+  "#{prefix}#{question}#{suffix}"
 end
 
 def create_fake_ballot(org, category:, isActive:)
@@ -31,7 +33,8 @@ def create_fake_ballot(org, category:, isActive:)
     Faker::Time.between from: created_at, to: $simulation.ended_at
   end
 
-  encrypted_question = $simulation.encrypt hipster_ipsum_ballot_question
+  encrypted_question = $simulation.encrypt hipster_ipsum_ballot_question(
+    QUESTION_PREFIXES[category], QUESTION_SUFFIX)
 
   # Pick a random member who had joined by that time to be the creator
   creator = org.users.where(joined_at: ...created_at).sample
@@ -45,9 +48,13 @@ end
 
 inactive_yes_no_ballot_count = BALLOTS_DISTRIBUTION.call
 active_yes_no_ballot_count = BALLOTS_DISTRIBUTION.call
+inactive_multiple_choice_ballot_count = BALLOTS_DISTRIBUTION.call
+active_multiple_choice_ballot_count = BALLOTS_DISTRIBUTION.call
 ballot_count = [
   inactive_yes_no_ballot_count,
   active_yes_no_ballot_count,
+  inactive_multiple_choice_ballot_count,
+  active_multiple_choice_ballot_count,
 ].sum
 
 print "\tCreating #{ballot_count} ballots... "
@@ -56,11 +63,19 @@ start_time = Time.now
 org = User.find($simulation.founder_id).org
 
 inactive_yes_no_ballot_count.times do
-  create_fake_ballot org, category: 'yes_no', isActive: false
+  create_fake_ballot org, category: :yes_no, isActive: false
 end
 
 active_yes_no_ballot_count.times do
-  create_fake_ballot org, category: 'yes_no', isActive: true
+  create_fake_ballot org, category: :yes_no, isActive: true
+end
+
+inactive_multiple_choice_ballot_count.times do
+  create_fake_ballot org, category: :multiple_choice, isActive: false
+end
+
+active_multiple_choice_ballot_count.times do
+  create_fake_ballot org, category: :multiple_choice, isActive: true
 end
 
 puts "Completed in #{(Time.now - start_time).round 3} s"
