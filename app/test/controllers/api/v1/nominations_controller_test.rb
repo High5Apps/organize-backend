f4c6e5c6176b8eb75055eeea36d1905f95b4c306
Julian Tigler <High5Apps@gmail.com>
2024-03-12 10:40:56 +0200

    Refactor nominations_controller_test in preparation for update tests
    - Rename @user to @nominator
    - Rename @authorized_headers to @create_headers
    - Update tests will need the @nomination not to be destroyed, so needed to remove @nomination.destroy! from setup
    - Add destroy_template_nomination_for_create_params to make it more difficult to write false-positive create tests that really just trigger duplicate validation errors instead of the actual error under test

--- app/test/controllers/api/v1/nominations_controller_test.rb
+++ app/test/controllers/api/v1/nominations_controller_test.rb
@@ -3,19 +3,18 @@ require "test_helper"
 class Api::V1::NominationsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @nomination = nominations(:election_one_choice_four)
-    @nomination.destroy!
-    @params = { nomination: @nomination.as_json }
 
-    @user = @nomination.nominator
-    setup_test_key(@user)
-    @authorized_headers = authorized_headers(@user, Authenticatable::SCOPE_ALL)
+    @nominator = @nomination.nominator
+    setup_test_key(@nominator)
+    @create_headers = authorized_headers @nominator, Authenticatable::SCOPE_ALL
   end
 
   test 'should create with valid params' do
+    params = destroy_template_nomination_for_create_params
     assert_difference 'Nomination.count', 1 do
-      post api_v1_ballot_nominations_url(@nomination.ballot),
-        headers: @authorized_headers,
-        params: @params
+      post(api_v1_ballot_nominations_url(@nomination.ballot),
+        headers: @create_headers,
+        params:)
       assert_response :created
     end
 
@@ -24,44 +23,48 @@ class Api::V1::NominationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should not create with invalid authorization' do
+    params = destroy_template_nomination_for_create_params
     assert_no_difference 'Nomination.count' do
-      post api_v1_ballot_nominations_url(@nomination.ballot),
-        headers: authorized_headers(@user,
+      post(api_v1_ballot_nominations_url(@nomination.ballot),
+        headers: authorized_headers(@nominator,
           Authenticatable::SCOPE_ALL,
           expiration: 1.second.ago),
-        params: @params
+        params:)
       assert_response :unauthorized
     end
   end
 
   test 'should not create with invalid params' do
+    params = destroy_template_nomination_for_create_params.merge nomination: {}
     assert_no_difference 'Nomination.count' do
-      post api_v1_ballot_nominations_url(@nomination.ballot),
-        headers: @authorized_headers,
-        params: { nomination: {} }
+      post(api_v1_ballot_nominations_url(@nomination.ballot),
+        headers: @create_headers,
+        params:)
     end
 
     assert_response :unprocessable_entity
   end
 
   test 'should not create if user is not in an org' do
-    @user.update!(org: nil)
-    assert_nil @user.reload.org
+    params = destroy_template_nomination_for_create_params
+    @nominator.update!(org: nil)
+    assert_nil @nominator.reload.org
 
     assert_no_difference 'Nomination.count' do
-      post api_v1_ballot_nominations_url(@nomination.ballot),
-        headers: @authorized_headers,
-        params: @params
+      post(api_v1_ballot_nominations_url(@nomination.ballot),
+        headers: @create_headers,
+        params:)
     end
 
     assert_response :not_found
   end
 
   test 'should not create on a nonexistent ballot' do
+    params = destroy_template_nomination_for_create_params
     assert_no_difference 'Nomination.count' do
-      post api_v1_ballot_nominations_url('bad-ballot-id'),
-        headers: @authorized_headers,
-        params: @params
+      post(api_v1_ballot_nominations_url('bad-ballot-id'),
+        headers: @create_headers,
+        params:)
     end
 
     assert_response :not_found
@@ -69,14 +72,24 @@ class Api::V1::NominationsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should not create if ballot belongs to another Org' do
     ballot_in_another_org = ballots(:two)
-    assert_not_equal @user.org, ballot_in_another_org.org
+    assert_not_equal @nominator.org, ballot_in_another_org.org
 
+    params = destroy_template_nomination_for_create_params
     assert_no_difference 'Nomination.count' do
-      post api_v1_ballot_nominations_url(ballot_in_another_org),
-        headers: @authorized_headers,
-        params: @params
+      post(api_v1_ballot_nominations_url(ballot_in_another_org),
+        headers: @create_headers,
+        params:)
     end
 
     assert_response :not_found
+  end
+
+  private
+
+  def destroy_template_nomination_for_create_params
+    # Destroy the existing nomination to prevent triggering duplicate
+    # validation errors
+    @nomination.destroy!
+    { nomination: @nomination.as_json }
   end
 end
