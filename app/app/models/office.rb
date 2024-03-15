37944ca53f46cfbb7743c9ff9fa33266c732b940
Julian Tigler <High5Apps@gmail.com>
2024-03-15 10:12:51 +0200

    Fix Office was available when awaiting acceptance by a winner
    - Without this, offices would become available again in the period between when voting ends and when the winner accepts the vote

--- app/app/models/office.rb
+++ app/app/models/office.rb
@@ -32,19 +32,29 @@ class Office
       raise 'office param must be a string'
     end
 
+    now = Time.now
+    elections = org.ballots.election
+
     # It's not open if there's already an active election for the office
-    active_election_offices = org.ballots.election.active_at(Time.now)
-      .pluck(:office)
+    active_election_offices = elections.active_at(now).pluck(:office)
+
+    # It's not open if there's an election awaiting term acceptance by a winner
+    elections_awaiting_acceptance = elections
+      .in_term_acceptance_period(now)
+      .to_a
+      .filter { |election| election.winners.count > 0 }
+      .map {|election| election[:office] }
 
     # It's not open if there's already a term that's outside of its cooldown
     # period. (Doesn't apply to stewards, since there can be multiple.)
     filled_offices_outside_cooldown = org.terms
-      .active_at(Time.now + Term::COOLDOWN_PERIOD)
+      .active_at(now + Term::COOLDOWN_PERIOD)
       .pluck(:office) - ['steward']
 
     office_types = Office::TYPE_STRINGS
     open_offices = \
-      office_types - filled_offices_outside_cooldown - active_election_offices
+      office_types - filled_offices_outside_cooldown - active_election_offices \
+      - elections_awaiting_acceptance
 
     offices = office_types.map do |type|
       open = open_offices.include?(type)
--- app/test/models/office_test.rb
+++ app/test/models/office_test.rb
@@ -41,6 +41,34 @@ class OfficeTest < ActiveSupport::TestCase
     end
   end
 
+  test 'availability should not be open if there is an election awaiting term acceptance by a winner' do
+    org = orgs :one
+    election = destroy_all_elections_but_one org
+
+    travel_to election.voting_ends_at + 1.second do
+      assert_not_empty election.winners
+
+      Office::TYPE_STRINGS.each do |office|
+        election.update!(office:)
+        assert_availability_open_except_for([office], org:)
+      end
+    end
+  end
+
+  test 'availability should not be affected if there is an election awaiting term acceptance but no winner exists' do
+    org = orgs :one
+    election = destroy_all_elections_but_one org
+    election.votes.destroy_all
+    travel_to election.voting_ends_at + 1.second do
+      assert_empty election.winners
+
+      Office::TYPE_STRINGS.each do |office|
+        election.update!(office:)
+        assert_availability_open_except_for([], org:)
+      end
+    end
+  end
+
   test 'availability for non-stewards should not be open if there is a term outside cooldown' do
     (Office::TYPE_STRINGS - ['founder', 'steward']).each do |office|
       attributes = @term.attributes.merge id: nil, office:, user_id: @user.id
@@ -73,9 +101,23 @@ class OfficeTest < ActiveSupport::TestCase
 
   private
 
-  def assert_availability_open_except_for offices
+  def assert_availability_open_except_for offices, org: nil
+    org ||= @org
     assert_equal offices.sort,
-      Office.availability_in(@org)
+      Office.availability_in(org)
         .filter { |o| !o[:open] }.map { |o| o[:type] }.sort
+  end
+
+  def destroy_all_elections_but_one org
+    org.terms.each { |term| term.destroy! }
+    election = org.ballots.election.first
+    org.ballots.election.where.not(id: election.id).each do |election|
+      election.votes.destroy_all
+      election.candidates.destroy_all
+      election.nominations.destroy_all
+      election.destroy!
+    end
+
+    election
   end
 end
