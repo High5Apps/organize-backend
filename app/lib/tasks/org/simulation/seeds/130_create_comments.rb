b223f4c29d7774036cb5e1fd042681c1460dadd2
Julian Tigler <High5Apps@gmail.com>
2024-04-21 17:39:19 +0300

    Fix org:sim error due to no available commenters
    - Based on the logs below, it seems like no member had joined by comment_time
    - If this happens again in the future, just skip the comment
    ```
    	Creating roughly 83 comments... bin/rails aborted!
    ActiveRecord::RecordInvalid: Validation failed: User must exist, User can't be blank (ActiveRecord::RecordInvalid)
    /usr/src/app/lib/tasks/org/simulation/seeds/130_create_comments.rb:75:in `block (3 levels) in <main>'
    /usr/src/app/lib/tasks/org/simulation/seeds/130_create_comments.rb:74:in `block (2 levels) in <main>'
    /usr/src/app/lib/tasks/org/simulation/seeds/130_create_comments.rb:60:in `times'
    /usr/src/app/lib/tasks/org/simulation/seeds/130_create_comments.rb:60:in `block in <main>'
    /usr/src/app/lib/tasks/org/simulation/seeds/130_create_comments.rb:50:in `<main>'
    /usr/src/app/lib/tasks/org.rake:68:in `load'
    /usr/src/app/lib/tasks/org.rake:68:in `block (3 levels) in <main>'
    /usr/src/app/lib/tasks/org.rake:68:in `each'
    /usr/src/app/lib/tasks/org.rake:68:in `block (2 levels) in <main>'
    Tasks: TOP => org:sim => org:simulation
    (See full trace by running task with --trace)
    ```

--- app/lib/tasks/org/simulation/seeds/130_create_comments.rb
+++ app/lib/tasks/org/simulation/seeds/130_create_comments.rb
@@ -65,6 +65,7 @@ posts.order(:created_at).each do |post|
 
     # Pick a random member who had joined by that time to be the commenter
     commenter = users.joined_at_or_before(comment_time).sample
+    next unless commenter
 
     if rand < 0.8 && !potential_parent_comments.empty?
       parent = potential_parent_comments.sample
