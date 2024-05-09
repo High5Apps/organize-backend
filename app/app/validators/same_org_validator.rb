d5b9f3bb0384a1f8c8c4238e6fbe62bba0f42204
Julian Tigler <High5Apps@gmail.com>
2024-05-09 11:26:55 +0300

    Update SameOrgValidator to accept an Org itself
    - Previously, the value and other value needed to be an object that could call `.org`
    - Now they can still be that, but they can also be an Org itself (even though an Org has no `.org` method)

--- app/app/validators/same_org_validator.rb
+++ app/app/validators/same_org_validator.rb
@@ -3,12 +3,14 @@ class SameOrgValidator < ActiveModel::EachValidator
     if options[:with].instance_of? Symbol
       error_attribute = attribute
 
-      other_value_attribute = options[:with]
-      other_value = record.send(other_value_attribute)
+      other_attribute = options[:with]
+      other_value = record.send(other_attribute)
+      other_org = (other_attribute == :org) ? other_value : other_value&.org
     elsif options[:as].instance_of?(Proc) && options[:name].instance_of?(String)
       error_attribute = :base
 
       other_value = options[:as].call(record)
+      other_org = other_value&.org
     else
       raise 'unexpected options'
     end
@@ -18,7 +20,9 @@ class SameOrgValidator < ActiveModel::EachValidator
       message = "#{options[:name]} #{message}"
     end
 
-    unless value&.org && (value&.org == other_value&.org)
+    org = (attribute == :org) ? value : value&.org
+
+    unless org && (org == other_org)
       record.errors.add error_attribute, message
     end
   end
