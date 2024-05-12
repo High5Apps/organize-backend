dfaef531371206a360ebb3ee844ae44843dca56f
Julian Tigler <High5Apps@gmail.com>
2024-05-12 23:49:06 +0300

    Add create post and get posts to load test
    - Add proxy recordings for requests in the Discuss tab
    - Add util.groovy file with e2eEncrypt()
      - I tried to use props internally so I didn't need to always include props.get('groupSecretBase64'), but props (and other helpers like OUT or vars) can't be used inside the util.groovy file
      - https://stackoverflow.com/a/67277183/2421313
      - https://jmeter.apache.org/usermanual/get-started.html#override
      - https://jmeter.apache.org/usermanual/properties_reference.html#groovy
      - https://jmeter.apache.org/usermanual/functions.html#__groovy
    - Use e2eEncrypt util instead of a whole JSR223 PreProcessor for Create Org name and member definition
    - Update bin/jmeter and bin/load-test arguments to use util.groovy for the groovy.utilities prop
    - Use time helper for Get General Posts created_at_or_before param
      - https://jmeter.apache.org/usermanual/functions.html#__time

---
+++ jmeter/util.groovy
@@ -0,0 +1,33 @@
+import groovy.json.JsonBuilder
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.io.Encoders
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
+class EncryptedMessage {
+    String c
+    String n
+    String t
+}
+
+def e2eEncrypt(String message, String symmetricKeyBase64) {
+  def groupSecret = Decoders.BASE64.decode(symmetricKeyBase64)
+
+  def secretKey = new SecretKeySpec(groupSecret, 'AES')
+  def cipher = Cipher.getInstance('AES/GCM/NoPadding')
+  cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+
+  def messageBytes = message.getBytes(StandardCharsets.UTF_8)
+  def ciphertextAndIntegrityCheck = cipher.doFinal(messageBytes)
+
+  def totalLength = ciphertextAndIntegrityCheck.length
+  def ciphertextLength = totalLength - 16
+  def ciphertext = Encoders.BASE64.encode(Arrays.copyOfRange(ciphertextAndIntegrityCheck, 0, ciphertextLength))
+  def integrityCheck = Encoders.BASE64.encode(Arrays.copyOfRange(ciphertextAndIntegrityCheck, ciphertextLength, totalLength))
+  def initializationVector = Encoders.BASE64.encode(cipher.getIV())
+
+  def encryptedMessage = new EncryptedMessage(c: ciphertext, n: initializationVector, t: integrityCheck)
+  return new JsonBuilder(encryptedMessage).toString()
+}
