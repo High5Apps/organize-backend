20e31a01a2bfa39164e2822f3c51860dd61154d7
Julian Tigler <High5Apps@gmail.com>
2024-05-16 10:14:50 +0300

    Fix org:sim was using flaggable creator as flag creator
    - Previously it simulated as though the flaggable creator was also the flag creator, which is unrealistic because it's unlikely for people to flag their own content
    - Now it uses a random member who had joined by the time the flag was created

--- app/lib/tasks/org/simulation/seeds/150_create_flags.rb
+++ app/lib/tasks/org/simulation/seeds/150_create_flags.rb
@@ -1,7 +1,7 @@
 FLAGGED_DOWNVOTES_FRACTION = 0.03
 FLAGGED_VOTES_FRACTION = 0.01
 
-def create_flags(relation, flaggable_name, flagged_fraction)
+def create_flags(relation, flaggable_name, flagged_fraction, org)
   expected_flag_count = (relation.count * flagged_fraction).round
   print "\tCreating roughly #{expected_flag_count} flags on #{flaggable_name.pluralize}... "
   start_time = Time.now
@@ -12,10 +12,14 @@ def create_flags(relation, flaggable_name, flagged_fraction)
     next unless rand < flagged_fraction
 
     created_at = flaggable.created_at + 3.seconds
+
+    # Pick a random member who had joined by that time to be the flag_creator
+    flag_creator = org.users.joined_at_or_before(created_at).sample
+
     values.push({
       flaggable_id: flaggable[flaggable_name.foreign_key] || flaggable.id,
       flaggable_type: flaggable_name,
-      user_id: flaggable.user_id,
+      user_id: flag_creator.id,
       created_at: created_at,
       updated_at: created_at,
     })
@@ -32,10 +36,10 @@ downvotes = org.upvotes.where(value: -1)
 posts_without_candidacy_announcements = downvotes.joins(:post)
   .where(post: { candidate_id: nil })
 create_flags posts_without_candidacy_announcements,
-  'Post', FLAGGED_DOWNVOTES_FRACTION
+  'Post', FLAGGED_DOWNVOTES_FRACTION, org
 
 comments = downvotes.joins(:comment)
-create_flags comments, 'Comment', FLAGGED_DOWNVOTES_FRACTION
+create_flags comments, 'Comment', FLAGGED_DOWNVOTES_FRACTION, org
 
 non_election_votes = org.ballots.not_election.joins(:votes)
-create_flags non_election_votes, 'Ballot', FLAGGED_VOTES_FRACTION
+create_flags non_election_votes, 'Ballot', FLAGGED_VOTES_FRACTION, org
