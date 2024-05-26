267c92f17c0751ebb36c40cf39d6a769c877d047
Julian Tigler <High5Apps@gmail.com>
2024-05-26 10:35:34 +0300

    Add ModerationEvent::Query
    - Responds to created_at_or_before param
    - Orders by most recently created

---
+++ app/app/models/moderation_event/query.rb
@@ -0,0 +1,29 @@
+class ModerationEvent::Query
+  ALLOWED_ATTRIBUTES = [
+    :action,
+    :created_at,
+    :id,
+    :moderatable_id,
+    :moderatable_type,
+    :user_id,
+    'users.pseudonym' => :user_pseudonym,
+  ].freeze
+
+  def self.build(initial_moderation_events, params={})
+    return ModerationEvent.none unless initial_moderation_events
+
+    now = Time.now
+    now_iso8601 = now.iso8601(6)
+
+    created_at_or_before_param = params[:created_at_or_before] || now_iso8601
+    created_at_or_before = Time.iso8601(created_at_or_before_param.to_s).utc
+
+    moderation_events = initial_moderation_events
+      .created_at_or_before(created_at_or_before)
+      .joins(:user)
+      .select(ALLOWED_ATTRIBUTES)
+      .order(created_at: :desc)
+
+    moderation_events
+  end
+end
---
+++ app/test/models/moderation_event/query_test.rb
@@ -0,0 +1,47 @@
+class ModerationEventQueryTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:one)
+  end
+
+  test 'should respect initial_moderation_events' do
+    org = @user.org
+    assert_not_equal ModerationEvent.count, org.moderation_events.count
+    moderation_events = ModerationEvent::Query.build org.moderation_events
+    assert moderation_events.all? { |me| me.user.org == org }
+  end
+
+  test 'should only include expected attributes' do
+    moderation_events = ModerationEvent::Query.build ModerationEvent.all
+    moderation_events.each do |moderation_event|
+      assert_pattern do
+        moderation_event.as_json.with_indifferent_access => {
+          action: String,
+          created_at: String,
+          id: String,
+          moderatable_id: String,
+          moderatable_type: String,
+          user_id: String,
+          user_pseudonym: String,
+          **nil
+        }
+      end
+    end
+  end
+
+  test 'should respect created_at_or_before param' do
+    event = moderation_events :one
+    events = ModerationEvent::Query.build ModerationEvent.all,
+      created_at_or_before: event.created_at.iso8601(6)
+    assert_not_equal ModerationEvent.count, events.to_a.count
+    assert_not_empty events
+    assert_equal ModerationEvent.created_at_or_before(event.created_at).sort,
+      events.sort
+  end
+
+  test 'should order by most recently created' do
+    event_created_ats = ModerationEvent::Query.build(ModerationEvent.all)
+      .pluck(:created_at)
+    expected = ModerationEvent.order(created_at: :desc).pluck(:created_at)
+    assert_equal expected, event_created_ats
+  end
+end
