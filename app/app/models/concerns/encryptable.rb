9f87802d2094d68748ef081b7c414f7ef6c17668
Julian Tigler <High5Apps@gmail.com>
2024-08-01 17:26:16 -0700

    Simplify encryptable to use validates_presence_of

--- app/app/models/concerns/encryptable.rb
+++ app/app/models/concerns/encryptable.rb
@@ -4,20 +4,12 @@ module Encryptable
   class_methods do
     def has_encrypted(attribute, present: false, max_length: nil)
       encrypted_attribute_name = "encrypted_#{attribute}"
-      validate_presence_method_name = "validate_#{attribute}_present"
       validate_max_length_method_name = "validate_#{attribute}_max_length"
 
-      validate validate_presence_method_name.to_sym if present
       validate validate_max_length_method_name.to_sym if max_length
+      validates_presence_of encrypted_attribute_name if present
 
       serialize encrypted_attribute_name, coder: EncryptedMessage
-
-      define_method(validate_presence_method_name) do
-        length = send(encrypted_attribute_name).decoded_ciphertext_length
-        unless length > 0
-          errors.add(encrypted_attribute_name.to_sym, "can't be blank")
-        end
-      end
 
       define_method(validate_max_length_method_name) do
         length = send(encrypted_attribute_name).decoded_ciphertext_length
