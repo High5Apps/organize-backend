0196b6d605706ad9dd1b1eca25c795f66c651e71
Julian Tigler <High5Apps@gmail.com>
2024-09-23 17:16:51 -0700

    Add simple controller test for home page

--- site/test/controllers/static_pages_controller_test.rb
+++ site/test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get privacy" do
     get privacy_url
     assert_response :success
