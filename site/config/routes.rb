f8c4354c6dd184b26f26780a87ced34f33042410
Julian Tigler <High5Apps@gmail.com>
2024-10-31 14:53:26 -0700

    Simplify site routes
    - https://api.rubyonrails.org/v7.2.2/classes/ActionDispatch/Routing/Mapper/Scoping.html#method-i-controller

--- site/config/routes.rb
+++ site/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root to: "static_pages#home"
-  get "about", to: "static_pages#about"
-  get "privacy", to: "static_pages#privacy"
-  get "terms", to: "static_pages#terms"
+  controller "static_pages" do
+    root "home"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    get "about"
+    get "privacy"
+    get "terms"
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -13,7 +14,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
