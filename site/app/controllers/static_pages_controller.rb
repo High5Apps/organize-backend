7a38a7eccdf45a46730211e55ed1fdb7058c4505
Julian Tigler <High5Apps@gmail.com>
2024-11-18 13:56:50 -0800

    Remove unneeded methods from StaticPagesController

--- site/app/controllers/static_pages_controller.rb
+++ site/app/controllers/static_pages_controller.rb
@@ -4,31 +4,7 @@ class StaticPagesController < ApplicationController
   def about
   end
 
-  def blog
-  end
-
-  def blog_aeiou_framework
-  end
-
-  def blog_tips_for_organic_leader_identification
-  end
-
-  def blog_tips_for_organizing_conversations
-  end
-
-  def blog_tips_for_starting_a_union
-  end
-
-  def frequently_asked_questions
-  end
-
-  def home
-  end
-
   def privacy
-  end
-
-  def structure_tests
   end
 
   def terms
