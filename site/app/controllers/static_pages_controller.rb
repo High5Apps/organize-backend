807ab519abc629e6088215e966482e243d4e9175
Julian Tigler <High5Apps@gmail.com>
2025-01-24 18:54:03 -0800

    Cache site static pages
    - Add dependency on actionpack-page_caching
      - https://github.com/rails/actionpack-page_caching
    - Needed to `chown` the `public` directory from `root` to `rails` to prevent a permissions error when the page cache attempted to read or write to the cache files
      - `Errno::EACCES (Permission denied @ rb_sysopen - /rails/public/blog.html)`
      - https://github.com/rails/actionpack-page_caching/issues/72
    - Despite the wiki below stating that I'd need to update my nginx.conf to serve the cache files, that step was not strictly required, since `config.public_file_server.enabled` is true by default.
      - https://github.com/rails/actionpack-page_caching/wiki#nginx
      - https://guides.rubyonrails.org/configuring.html#config-public-file-server-enabled
      - I confirmed via logging and inspection of the csrf token that the cached pages are successfully served without the rails server recreating the page (after the first time).
      - In a follow-up commit, I will disable the rails public_file_server flag in favor of nginx serving those public files
    - I verified that the entire page cache is effectively invalidated on redeployment/recreation of the site container. Since the public folder is not backed up to any named volume, all changes made to it while the container is running (e.g. page cache entries) are blown away when the container is recreated.

--- site/app/controllers/static_pages_controller.rb
+++ site/app/controllers/static_pages_controller.rb
@@ -1,6 +1,21 @@
 class StaticPagesController < ApplicationController
   before_action :set_email, only: [ :about, :privacy, :terms ]
 
+  caches_page :about,
+    :aeiou_framework,
+    :blog,
+    :community_allies,
+    :frequently_asked_questions,
+    :home,
+    :negotiations,
+    :privacy,
+    :structure_tests,
+    :terms,
+    :tips_for_organic_leader_identification,
+    :tips_for_organizing_conversations,
+    :tips_for_starting_a_union,
+    :union_busting_defenses,
+
   def about
   end
 
