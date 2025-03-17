597a5f561284393cb6abe26f60cf3d6aefc8e785
Julian Tigler <High5Apps@gmail.com>
2025-03-17 11:55:18 -0700

    Update site title based on page title length
    - When page title is short enough, use "Organize: Modern Labor Unions"
    - When page title is so long that the full title would be truncated, given the 70 character limit, just use "Organize"
    - Add a meta-tags initializer that truncates the site title before truncating the page title
      - https://github.com/kpumuk/meta-tags?tab=readme-ov-file#configuration

--- site/app/helpers/application_helper.rb
+++ site/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  SITE_TITLE = 'Organize: Modern Labor Unions'
+  SITE_TITLE_SHORT = 'Organize'
+
+  def site_title
+    # Subtract 3 more for ' | ' between site and page title
+    (title&.size || 0) > (MetaTags.config.title_limit - SITE_TITLE.length - 3) ?
+      SITE_TITLE_SHORT : SITE_TITLE
+  end
 end
---
+++ site/config/initializers/meta_tags.rb
@@ -0,0 +1,3 @@
+MetaTags.configure do |config|
+  config.truncate_site_title_first = true
+end
