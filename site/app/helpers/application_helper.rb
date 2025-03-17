909c10a6f621df0c72c04952476efcc2a52df387
Julian Tigler <High5Apps@gmail.com>
2025-03-17 15:46:32 -0700

    Fix site meta urls sometimes had IP address instead of hostname
    - Sometimes a bot is the first one to hit a page, and bots usually hit the IP address directly, instead of the hostname. Then the page is cached with the IP address in the meta url, messing up the canonical URL and image_src URLs
    - This caused social sites to not show the image_src, since https://12.34.56.78/assets/[slug].png failed the cert check, since the cert is only valid on getorganize.app, not the on the bare IP address
    - The fix was to manually set the host in asset_host config and in full_url_for as the host directly
      - Setting it in asset_host config makes it so that I don't need to manually set it for every image_url
    - I initially tried setting the Host header to be getorganize.app in nginx.conf, but that didn't work
    - I also tried using $host instead of $http_host in rails-proxy.conf, but that didn't work either

--- site/app/helpers/application_helper.rb
+++ site/app/helpers/application_helper.rb
@@ -2,6 +2,10 @@ module ApplicationHelper
   SITE_TITLE = 'Organize: Modern Labor Unions'
   SITE_TITLE_SHORT = 'Organize'
 
+  def canonical_url
+    full_url_for host: 'getorganize.app', port: nil, protocol: 'https'
+  end
+
   def site_title
     # Subtract 3 more for ' | ' between site and page title
     (title&.size || 0) > (MetaTags.config.title_limit - SITE_TITLE.length - 3) ?
