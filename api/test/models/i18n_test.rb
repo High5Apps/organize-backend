bf3451bad7e0be7ef52f1b1a3b3898c97adfe125
Julian Tigler <High5Apps@gmail.com>
2025-06-06 11:28:00 -0700

    Translate extracted strings to Spanish (en-US)

--- api/test/models/i18n_test.rb
+++ api/test/models/i18n_test.rb
@@ -1,23 +1,32 @@
 require "test_helper"
 
 class I18nTest < ActiveSupport::TestCase
+  setup do
+    @non_default_locales = I18n.available_locales - [I18n.default_locale]
+  end
+
   test 'model names should be localized to Spanish' do
+    skip_set = [Org].to_set
+
     ApplicationRecord.descendants.each do |klass|
-      assert_not_equal klass.model_name.human(locale: :es),
-        klass.model_name.human
+      next if skip_set.include? klass
+
+      @non_default_locales.each do |locale|
+        assert_not_equal klass.model_name.human(locale:),
+          klass.model_name.human
+      end
     end
   end
 
   test 'attribute names should be localized to Spanish' do
-    skip_set = ['created_at', 'id', 'updated_at'].to_set
-    non_default_locales = I18n.available_locales - [I18n.default_locale]
+    skip_set = ['created_at', 'id', 'org', 'updated_at'].to_set
 
     ApplicationRecord.descendants.each do |klass|
       klass.new.attributes.keys.each do |attribute|
         key = attribute.delete_suffix '_id'
         next if skip_set.include? key
 
-        non_default_locales.each do |locale|
+        @non_default_locales.each do |locale|
           assert_not_equal klass.human_attribute_name(key, locale:),
             klass.human_attribute_name(key)
         end
