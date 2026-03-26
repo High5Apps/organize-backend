359744102ca20f188ac48a5b1cba723a6dc07ab7
Julian Tigler <High5Apps@gmail.com>
2026-03-26 16:33:48 -0700

    Add Compose Email button to new Org notification
    - Before 915b968, when notifications were backed by ntfy.sh, I didn't want to include any sensitive info in the notification, such as email or verification code
    - Now that the notification is backed by the monitor-notify service, that's no longer a problem
    - This feature allows me to send the welcome email much faster than before, since I don't need to manually look up the email or verification code, but it still allows me to make a judgement call on whether the person/email is a bot or not before sending the email
    - Encode mailto body and subject using ERB::Util.url_encode, since URI.encode is deprecated and URI.encode_www_form_component uses + instead of %20
      - https://stackoverflow.com/a/59398072/2421313

--- api/app/jobs/new_org_notification_job.rb
+++ api/app/jobs/new_org_notification_job.rb
@@ -1,17 +1,45 @@
 class NewOrgNotificationJob < ApplicationJob
   queue_as :default
 
+  BODY_FORMAT = %(
+Welcome to Organize!
+
+I'm Julian, and I'm here to help you form your own union.
+
+To help me know you're real, can you tell me a little bit about the issues at your workplace?
+
+If you just want to try out the app ASAP, your verification code is: %{verification_code}, but please reply to this email when you get a chance.
+
+And if you've got any questions for me, feel free to ask!
+
+Thanks,
+Julian Tigler
+https://getorganize.app/blog/tips_for_starting_a_union
+  ).strip
   NOTIFICATION_URL = "http://monitor-notify/org_created"
   NOTIFICATION_TITLE = "New Org Created"
 
-  def perform(*args)
+  def perform(org)
     HTTParty.post(
       NOTIFICATION_URL,
+      body: org.email,
       headers: {
+        Actions: self.class.actions(org),
         Authorization: self.class.authorization(ENV.fetch("NTFY_TOKEN")),
         Title: NOTIFICATION_TITLE,
       }
     )
+  end
+
+  def self.actions(org)
+    email, verification_code = org.email, org.verification_code
+    mail_to = "mailto:%{email}?subject=%{subject}&body=%{body}" % {
+      body: ERB::Util.url_encode(BODY_FORMAT % {verification_code:}),
+      email:,
+      subject: ERB::Util.url_encode('Organize Verification'),
+    }
+
+    "view, Compose Email, #{mail_to}, clear=true"
   end
 
   def self.authorization(token)
--- api/test/jobs/new_org_notification_job_test.rb
+++ api/test/jobs/new_org_notification_job_test.rb
@@ -3,13 +3,17 @@ require "test_helper"
 class NewOrgNotificationJobTest < ActiveJob::TestCase
   FAKE_TOKEN = "my_user:tk_example_token_123456789098764"
 
+  setup do
+    @org = orgs(:one)
+  end
+
   test "should make a post request to NOTIFICATION_URL" do
     stub_notify = stub_notification_request
     assert_not_requested stub_notify
 
     with_env_var("NTFY_TOKEN", FAKE_TOKEN) do
       perform_enqueued_jobs do
-        NewOrgNotificationJob.perform_later
+        NewOrgNotificationJob.perform_later @org
       end
     end
 
@@ -21,7 +25,9 @@ class NewOrgNotificationJobTest < ActiveJob::TestCase
   def stub_notification_request
     stub_request(:post, NewOrgNotificationJob::NOTIFICATION_URL).
       with(
+        body: @org.email,
         headers: {
+          Actions: NewOrgNotificationJob::actions(@org),
           Authorization: NewOrgNotificationJob::authorization(FAKE_TOKEN),
           Title: NewOrgNotificationJob::NOTIFICATION_TITLE,
         }
