High5Apps/organize-backend

commit 81b73c40eb13722768183c04120de9c0e5e01de0

Rename NewOrgNotificationJob to OrgCreatedNotificationJob

api/app/controllers/v1/orgs_controller.rb
@@ -16,7 +16,7 @@ class V1::OrgsController < ApplicationController
   def create
     new_org = authenticated_user.build_org(create_or_update_params)
     if new_org.save && authenticated_user.save
-      NewOrgNotificationJob.perform_later new_org
+      OrgCreatedNotificationJob.perform_later new_org
       render json: { id: new_org.id }, status: :created
     else
       render_error :unprocessable_entity, new_org.errors.full_messages

api/test/controllers/v1/orgs_controller_test.rb
@@ -59,7 +59,7 @@ class V1::OrgsControllerTest < ActionDispatch::IntegrationTest
     assert_not_nil @user.reload.pseudonym
   end
 
-  test "should not enqueue a NewOrgNotificationJob on failed create" do
+  test "should not enqueue a OrgCreatedNotificationJob on failed create" do
     assert_no_enqueued_jobs
     post v1_orgs_url, headers: @authorized_headers, params: {
       org: @params[:org].except(:encrypted_name)
@@ -67,8 +67,8 @@ class V1::OrgsControllerTest < ActionDispatch::IntegrationTest
     assert_no_enqueued_jobs
   end
 
-  test "should enqueue a NewOrgNotificationJob on successful create" do
-    assert_enqueued_with(job: NewOrgNotificationJob) do
+  test "should enqueue a OrgCreatedNotificationJob on successful create" do
+    assert_enqueued_with(job: OrgCreatedNotificationJob) do
       post v1_orgs_url, headers: @authorized_headers, params: @params
     end
   end

api/app/jobs/org_created_notification_job.rb
@@ -1,4 +1,4 @@
-class NewOrgNotificationJob < ApplicationJob
+class OrgCreatedNotificationJob < ApplicationJob
   queue_as :default
 
   BODY_FORMAT = %(
@@ -17,7 +17,7 @@ Julian Tigler
 https://getorganize.app/blog/tips_for_starting_a_union
   ).strip
   NOTIFICATION_URL = "http://monitor-notify/org_created"
-  NOTIFICATION_TITLE = "New Org Created"
+  NOTIFICATION_TITLE = "Org Created"
 
   def perform(org)
     HTTParty.post(

api/test/jobs/org_created_notification_job_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class NewOrgNotificationJobTest < ActiveJob::TestCase
+class OrgCreatedNotificationJobTest < ActiveJob::TestCase
   FAKE_TOKEN = "my_user:tk_example_token_123456789098764"
 
   setup do
@@ -13,7 +13,7 @@ class NewOrgNotificationJobTest < ActiveJob::TestCase
 
     with_env_var("NTFY_TOKEN", FAKE_TOKEN) do
       perform_enqueued_jobs do
-        NewOrgNotificationJob.perform_later @org
+        OrgCreatedNotificationJob.perform_later @org
       end
     end
 
@@ -23,13 +23,13 @@ class NewOrgNotificationJobTest < ActiveJob::TestCase
   private
 
   def stub_notification_request
-    stub_request(:post, NewOrgNotificationJob::NOTIFICATION_URL).
+    stub_request(:post, OrgCreatedNotificationJob::NOTIFICATION_URL).
       with(
         body: @org.email,
         headers: {
-          Actions: NewOrgNotificationJob::actions(@org),
-          Authorization: NewOrgNotificationJob::authorization(FAKE_TOKEN),
-          Title: NewOrgNotificationJob::NOTIFICATION_TITLE,
+          Actions: OrgCreatedNotificationJob::actions(@org),
+          Authorization: OrgCreatedNotificationJob::authorization(FAKE_TOKEN),
+          Title: OrgCreatedNotificationJob::NOTIFICATION_TITLE,
         }
       )
   end

commit fe85c26641610e7dcdc4cca78d678da313fe9a9f

Send a notification when an Org is verified
- Similar to 40b3242
- Fix a/an typo introduced in 81b73c4

api/app/controllers/v1/orgs_controller.rb
@@ -47,6 +47,7 @@ class V1::OrgsController < ApplicationController
 
   def verify
     if @org.verify(params[:code])
+      OrgVerifiedNotificationJob.perform_later @org
       head :ok
     else
       render_error :forbidden, [t('.errors.invalid')]

api/app/jobs/org_verified_notification_job.rb
@@ -1,0 +1,8 @@
+class OrgVerifiedNotificationJob < NotificationJob
+
+  TOPIC = "org_verified"
+
+  def perform(org)
+    send_notification TOPIC, body: org.email
+  end
+end

api/test/controllers/v1/orgs_controller_test.rb
@@ -59,7 +59,7 @@ class V1::OrgsControllerTest < ActionDispatch::IntegrationTest
     assert_not_nil @user.reload.pseudonym
   end
 
-  test "should not enqueue a OrgCreatedNotificationJob on failed create" do
+  test "should not enqueue an OrgCreatedNotificationJob on failed create" do
     assert_no_enqueued_jobs
     post v1_orgs_url, headers: @authorized_headers, params: {
       org: @params[:org].except(:encrypted_name)
@@ -67,7 +67,7 @@ class V1::OrgsControllerTest < ActionDispatch::IntegrationTest
     assert_no_enqueued_jobs
   end
 
-  test "should enqueue a OrgCreatedNotificationJob on successful create" do
+  test "should enqueue an OrgCreatedNotificationJob on successful create" do
     assert_enqueued_with(job: OrgCreatedNotificationJob) do
       post v1_orgs_url, headers: @authorized_headers, params: @params
     end
@@ -294,5 +294,21 @@ class V1::OrgsControllerTest < ActionDispatch::IntegrationTest
       headers: authorized_headers(@user_in_org, Authenticatable::SCOPE_ALL),
       params: { code: @org.verification_code }
     assert_response :forbidden
+  end
+
+  test "should not enqueue an OrgVerifiedNotificationJob on failed verify" do
+    assert_no_enqueued_jobs
+    post v1_verify_url,
+      headers: authorized_headers(@user_in_org, Authenticatable::SCOPE_ALL),
+      params: { code: 'invalid' }
+    assert_no_enqueued_jobs
+  end
+
+  test "should enqueue an OrgVerifiedNotificationJob on successful verify" do
+    assert_enqueued_with(job: OrgVerifiedNotificationJob) do
+      post v1_verify_url,
+        headers: authorized_headers(@user_in_org, Authenticatable::SCOPE_ALL),
+        params: { code: @org.verification_code }
+    end
   end
 end

api/test/jobs/org_verified_notification_job_test.rb
@@ -1,0 +1,36 @@
+require "test_helper"
+
+class OrgVerifiedNotificationJobTest < ActiveJob::TestCase
+  FAKE_TOKEN = "my_user:tk_example_token_123456789098764"
+
+  setup do
+    @org = orgs(:one)
+  end
+
+  test "should send notification" do
+    stub_notify = stub_notification_request
+    assert_not_requested stub_notify
+
+    with_env_var("NTFY_TOKEN", FAKE_TOKEN) do
+      perform_enqueued_jobs do
+        OrgVerifiedNotificationJob.perform_later @org
+      end
+    end
+
+    assert_requested stub_notify
+  end
+
+  private
+
+  def stub_notification_request
+    url = NotificationJob::notification_url(OrgVerifiedNotificationJob::TOPIC)
+    stub_request(:post, url).
+      with(
+        body: @org.email,
+        headers: {
+          Authorization: NotificationJob::authorization(FAKE_TOKEN),
+          Title: NotificationJob::title(OrgVerifiedNotificationJob::TOPIC),
+        }
+      )
+  end
+end